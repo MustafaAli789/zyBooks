e0033c3b7dbf6aeef322bbf4bd2d2cde0120ff55
Mustafa Ali <alimustafa0987@gmail.com>
2020-07-16 01:36:26 -0400

    Updates

---
+++ precache-manifest.74e534d3c8888d8f36b9845ffb9f0867.js
@@ -0,0 +1,78 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "ceee1a220011103e4c4e2289e992cb00",
+    "url": "/zyBooks/index.html"
+  },
+  {
+    "revision": "8a0ca062c5abe8e58318",
+    "url": "/zyBooks/static/css/main.1476a8b2.chunk.css"
+  },
+  {
+    "revision": "a8e856f77b8568cf70a4",
+    "url": "/zyBooks/static/js/2.0ecf8a43.chunk.js"
+  },
+  {
+    "revision": "89b0379e7bcda1a468d8b0343aeb4e53",
+    "url": "/zyBooks/static/js/2.0ecf8a43.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "8a0ca062c5abe8e58318",
+    "url": "/zyBooks/static/js/main.3da4f127.chunk.js"
+  },
+  {
+    "revision": "7ab5b5d1a2e5b1267b2e",
+    "url": "/zyBooks/static/js/runtime-main.3bedfe0a.js"
+  },
+  {
+    "revision": "9aa6f45860616979a1d880710d89d6be",
+    "url": "/zyBooks/static/media/HarryPotter.9aa6f458.png"
+  },
+  {
+    "revision": "1ed0b882a36daba889e7416e895524f5",
+    "url": "/zyBooks/static/media/bookNotFound.1ed0b882.png"
+  },
+  {
+    "revision": "2c485b6e406076bfdbca788a5771744a",
+    "url": "/zyBooks/static/media/cursedChild.2c485b6e.jpg"
+  },
+  {
+    "revision": "7de28438957af3d5cf43b8d20e60b0d8",
+    "url": "/zyBooks/static/media/enchantment.7de28438.jpg"
+  },
+  {
+    "revision": "3cd06e009c5a4e9f6bae9b442082a9b7",
+    "url": "/zyBooks/static/media/findYouInTheDark.3cd06e00.jpg"
+  },
+  {
+    "revision": "a87686a46a3d9692f442b1c968bbeaf6",
+    "url": "/zyBooks/static/media/findingMoana.a87686a4.jpg"
+  },
+  {
+    "revision": "c8f9f3b40fead5eb782050e6952dae5b",
+    "url": "/zyBooks/static/media/fromTheAshes.c8f9f3b4.jpg"
+  },
+  {
+    "revision": "def249f2a518b5e3343b122f4caae5b9",
+    "url": "/zyBooks/static/media/gameOfThrones.def249f2.jpg"
+  },
+  {
+    "revision": "02d52102a361e8a431738330e7aae96a",
+    "url": "/zyBooks/static/media/kingOfDrugs.02d52102.jpg"
+  },
+  {
+    "revision": "d7308b095997700c97779ca8c8060bcc",
+    "url": "/zyBooks/static/media/mirrorAndTheLight.d7308b09.jpg"
+  },
+  {
+    "revision": "a3f005aa0a30c6e87d9ea68a019017d2",
+    "url": "/zyBooks/static/media/piecesOfLight.a3f005aa.jpg"
+  },
+  {
+    "revision": "1f64431ed9c42f4679e4b18b766c5226",
+    "url": "/zyBooks/static/media/prophecy.1f64431e.jpg"
+  },
+  {
+    "revision": "c3a2f367fae98d21426404e466a95080",
+    "url": "/zyBooks/static/media/warAndPeace.c3a2f367.jpg"
+  }
+]);
